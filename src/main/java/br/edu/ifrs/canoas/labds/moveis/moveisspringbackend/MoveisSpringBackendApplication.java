42c3333a5e5d607f55f537ac90eef021a45fece8
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-06-28 09:51:31 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifrs/canoas/labds/moveis/moveisspringbackend/MoveisSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.canoas.labds.moveis.moveisspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveisSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveisSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrs/canoas/labds/moveis/moveisspringbackend/MoveisSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.canoas.labds.moveis.moveisspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveisSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
