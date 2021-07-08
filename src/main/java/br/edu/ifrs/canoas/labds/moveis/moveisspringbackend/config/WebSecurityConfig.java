3c522699e95a3196037fa63f23b6ac019818c664
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-07-08 20:43:30 -0300

    Changed dev database to h2

--- src/main/java/br/edu/ifrs/canoas/labds/moveis/moveisspringbackend/config/WebSecurityConfig.java
+++ src/main/java/br/edu/ifrs/canoas/labds/moveis/moveisspringbackend/config/WebSecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -38,6 +40,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers("/h2-console/**");
     }
 
     @Bean
