a05cc829929595d84ff6011414810ddf5238879e
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-08-21 15:09:45 -0300

    Added paging on shop

--- src/main/java/br/edu/ifrs/canoas/labds/moveis/moveisspringbackend/web/ShopController.java
+++ src/main/java/br/edu/ifrs/canoas/labds/moveis/moveisspringbackend/web/ShopController.java
@@ -4,11 +4,14 @@ import br.edu.ifrs.canoas.labds.moveis.moveisspringbackend.domain.Product;
 import br.edu.ifrs.canoas.labds.moveis.moveisspringbackend.dto.AddCartItemDTO;
 import br.edu.ifrs.canoas.labds.moveis.moveisspringbackend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * This controller must have routes to browse products in the shop
@@ -23,8 +26,13 @@ public class ShopController {
     private ProductService productService;
 
     @GetMapping({"/", "/shop"})
-    public String shopHome(Model model) {
-        model.addAttribute("products", productService.findAll());
+    public String shopHome(
+            @RequestParam(value = "p", defaultValue = "0") Integer pageNumber,
+            @RequestParam(value = "size", defaultValue = "10") Integer size,
+            Model model
+    ) {
+        Page<Product> page = productService.findPage(PageRequest.of(pageNumber, size));
+        model.addAttribute("page", page);
         return "shop/home";
     }
 
